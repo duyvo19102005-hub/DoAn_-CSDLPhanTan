539f6a3cae1c981092021df7a3bf8cef69020f7f
duyvo19102005-hub <duyvo19102005@gmail.com>
2026-05-05 19:43:41 +0700

    Add files via upload

---
+++ Models/ThiSinh.cs
@@ -0,0 +1,12 @@
+﻿namespace DoAn_CSDLPhanTan.Models
+{
+    public class ThiSinh
+    {
+        public string? MaTS { get; set; }
+        public string? HoTen { get; set; }
+        public double DiemToan { get; set; }
+        public double DiemVan { get; set; }
+        public double DiemAnh { get; set; }
+        public string? MaVung { get; set; }
+    }
+}
